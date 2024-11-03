31ea2fceeacdc813e0b1c438f79c1fab5191ab50
emreteth <81780199+emreteth@users.noreply.github.com>
2024-11-03 23:46:04 +0000

    yeni proje

---
+++ chat-app/server.js
@@ -0,0 +1,39 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Bir kullanıcı bağlandı');
+  
+  socket.on('set username', (username) => {
+    socket.username = username;
+    console.log(`Kullanıcı adı ayarlandı: ${username}`);
+  });
+
+  socket.on('chat message', (msg) => {
+    const user = socket.username || 'Anonim';
+    io.emit('chat message', { user, msg });
+  });
+
+  socket.on('disconnect', () => {
+    const user = socket.username
+    console.log(`${user}: ayrıldı`);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
